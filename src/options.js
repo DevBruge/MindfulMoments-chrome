27e1cc832a3ff962e3804cc95692e1d89abdbc25
Chris Faria <chrisfaria@gmail.com>
2015-11-10 19:07:20 -0500

    Added Chrome sync storage for all options: - Chrome Notification Enabled - boolean - Sound Enabled - boolean - Mindful messages - string arrad

--- src/options.js
+++ src/options.js
@@ -1,3 +1,19 @@
+/***********************
+ * TO DO
+ * - load default options into storage at first
+ *
+ */
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("add").addEventListener("click", addMsg);
+});
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("rem").addEventListener("click", remMsg);
+});
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("save").addEventListener("click", saveOptions);
+});
+
 // Add messages to the list box. This list is what will be displayed
 // at random times as mindful moments
 function addMsg() {
@@ -26,4 +42,36 @@ function remMsg(){
 	        listboxMsgs.remove(i);
 	    }
 	}
+}
+
+function saveOptions(){
+	var bChromeNotificationEnabled = document.getElementsByName('notify')[0].checked;
+	var bSoundEnabled = document.getElementById('sound').checked;
+	var optionsMsgs = document.getElementById('listboxMsgs').options;
+
+	// Add all options into an array since they can be used by chrome storage
+	var saMsgs = [];
+	for (var i = 0; i < optionsMsgs.length; i++) {
+		saMsgs.push(optionsMsgs[i].value);
+	};
+
+	chrome.storage.sync.set({'notifyOption': bChromeNotificationEnabled}, function() {
+        console.log("SET: The notification option value stored is: " + bChromeNotificationEnabled);
+    });
+    chrome.storage.sync.set({'soundOption': bSoundEnabled}, function() {
+        console.log("SET: The sound option value stored is: " + bSoundEnabled);
+    });
+    chrome.storage.sync.set({'mindfulMessages': saMsgs}, function() {
+        console.log("SET: The current mindful message count is: " + saMsgs.length);
+    });
+
+	//Test for get    
+    chrome.storage.sync.get(['notifyOption','soundOption','mindfulMessages'], function (items) {
+    	console.log("GET: The notification option value retreived is: " + items.notifyOption);
+        console.log("GET: The sound option value retreived is: " + items.soundOption);
+        console.log("GET: The mindful message count retreived is: " + items.mindfulMessages.length);
+        for (var i = 0; i < items.mindfulMessages.length; i++) {
+        	console.log("GET: Mindful message #" + (i+1) + ": " + items.mindfulMessages[i]);
+        };
+    });
 }
